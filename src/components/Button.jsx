8e97e361452c7cb43e9df3f28efdaffbdc00132a
GitGud031005 <phuc.nguyen0310@hcmut.edu.vn>
2025-09-05 08:47:09 +0700

    feat: Subscribe complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ label, iconURL, bgColor, textColor, borderColor}) => {
+const Button = ({ label, iconURL, bgColor, textColor, borderColor, fullWidth}) => {
     return (
         <button className={`flex justify-center 
         items-center gap-2 px-7 py-4 border
         font-[montserrat] text-lg leading-none rounded-full
+        ${fullWidth && 'w-full'}
         ${bgColor? `${bgColor} ${textColor} ${borderColor}` :
         `bg-coral-red text-white
         border-coral-red`}`}>
